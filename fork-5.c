d86eeeeb3ad35c8758f405510ccf175901c0c863
Awis Qirani <qiraniawis@gmail.com>
2020-10-24 02:34:01 +0800

    File for 2.3

---
+++ fork-5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void childTask()  {
+	printf("Salam, saya anak tau\n");
+}
+
+void parentTask() {
+	printf("Dan saya adalah bapaknya\n");
+}
+
+int main(void)    {
+	pid_t pid = fork();
+	
+	if(pid == 0) {
+		childTask();
+		exit(EXIST_SUCCESS);
+	}
+	else if(pid > 0) {
+		wait(NULL);
+		parentTask();
+	}
+	else {
+		printf("Unable to create child process.");
+	}
+	
+	return EXIST_SUCCESS;
+}
+
