63be0be36be1943bccaa671c05aa61907d9dcd90
Awis Qirani <qiraniawis@gmail.com>
2020-10-24 02:09:04 +0800

    Add new code: wait(NULL);

--- fork-4.c
+++ fork-4.c
@@ -1,16 +1,20 @@
 #include <stdlib.h>	/* needed to define exit() */
 #include <unistd.h>	/* needed  for fork() ad getpid() */
 #include <stdio.h>	/* needed for printf() */
+#include <sys/wait.h>
+
 
 int main (int argc, char **argv)	{
 	int pid;	/* process ID */
 
 	switch (pid = fork())	{
+
 	case 0:		/* a fork returns 0 to the child */
 		printf("I am the child process: pid=%d\n", getpid());
 		break;
 	
 	default:	/* a fork returns a pid to the parent */
+		wait(NULL);
 		printf("I am the parent process: pid=%d, child pid=%d\n", getpid(), pid);
 		break;
 
